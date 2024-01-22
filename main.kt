20ba84293c08033c0c4b662c79d662319430c9eb
bobser <144664653+bobser223@users.noreply.github.com>
2024-01-22 20:11:08 +0200

    fourth commit

--- main.kt
+++ main.kt
@@ -1,3 +1,14 @@
 for (int i = 0; i < 10; i++) {
     printf("Hello, world!\n");
 }
+
+
+// далі пайтон, бо вивчати нову мову програмування не в моїх планах на цю ніч (P.S. знаю що дефки спочатку, але тут 2 різні мови)
+
+def vivaMechMat():
+    print("Viva Mech-mat faculty")
+
+
+
+
+vivaMechMat()
